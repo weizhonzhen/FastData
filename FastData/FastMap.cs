ec4d0041fef0a655b638614b7c7a292ff81dfbd3
weizhonzhen <czdswzz@126.com>
2018-12-19 15:03:34 +0800

    Update FastMap.cs

--- FastData/FastMap.cs
+++ FastData/FastMap.cs
@@ -46,7 +46,7 @@ namespace FastData
                         Task.Factory.StartNew(() =>
                         {
                             var typeInfo = (temp as TypeInfo);
-                            if (typeInfo.Namespace.Contains(nameSpace))
+                            if (typeInfo.Namespace != null && typeInfo.Namespace.Contains(nameSpace))
                             {
                                 var key = string.Format("{0}.{1}", typeInfo.Namespace, typeInfo.Name);
 
@@ -125,7 +125,7 @@ namespace FastData
                     foreach (var temp in item.ExportedTypes)
                     {
                         var typeInfo = (temp as TypeInfo);
-                        if (typeInfo.Namespace.Contains(nameSpace))
+                        if (typeInfo.Namespace != null && typeInfo.Namespace.Contains(nameSpace))
                             BaseTable.Check(query, temp.Name, typeInfo.DeclaredProperties.ToList(), typeInfo.GetCustomAttributes().ToList());
                     }
                 }
