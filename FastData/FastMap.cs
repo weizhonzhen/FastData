840b98085a121bd82fd72edde81711c80f8c822a
weizhonzhen <czdswzz@126.com>
2019-09-08 19:50:26 +0800

    Update FastMap.cs

--- FastData/FastMap.cs
+++ FastData/FastMap.cs
@@ -721,7 +721,7 @@ namespace FastData
 
                                 i++;
                             }
-                            
+
                             if (temp.Attributes["db"] != null)
                                 db.Add(tempKey, temp.Attributes["db"].Value.ToStr());
                             param.Add(tempKey, tempParam);
@@ -792,8 +792,12 @@ namespace FastData
                     if (param != null)
                     {
                         var tempSql = new StringBuilder();
-                        foreach (var temp in param)
+                        foreach (var item in MapParam(name))
                         {
+                            if (!param.ToList().Exists(a => a.ParameterName.ToLower() == item.ToLower()))
+                                continue;
+                            var temp = param.ToList().Find(a => a.ParameterName.ToLower() == item.ToLower());
+
                             var paramKey = string.Format("{0}.{1}.{2}", name.ToLower(), temp.ParameterName.ToLower(), i);
                             var conditionKey = string.Format("{0}.{1}.condition.{2}", name.ToLower(), temp.ParameterName.ToLower(),i);
                             var conditionValueKey = string.Format("{0}.{1}.condition.value.{2}", name.ToLower(), temp.ParameterName.ToLower(), i);
