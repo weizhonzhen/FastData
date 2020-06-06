385f8df9a91027313776ea029de637d1e23c8478
weizhonzhen <czdswzz@126.com>
2020-06-06 22:07:35 +0800

    Update Parameter.cs

--- FastData/Base/Parameter.cs
+++ FastData/Base/Parameter.cs
@@ -1,9 +1,7 @@
-﻿using System;
 using System.Collections.Generic;
 using System.Data.Common;
-using FastData.Model;
 
-namespace FastData.Base
+namespace FastData.Core.Base
 {
     /// <summary>
     /// 标签：2015.9.6，魏中针
@@ -11,35 +9,6 @@ namespace FastData.Base
     /// </summary>
     internal static class Parameter
     {
-        #region DbParameter重先生成
-        /// <summary>
-        /// DbParameter重先生成
-        /// </summary>
-        /// <param name="param1"></param>
-        /// <param name="param2"></param>
-        /// <returns></returns>
-        public static List<DbParameter> ReNewParam(List<DbParameter> param, ConfigModel config)
-        {
-            return param;
-            var list = new List<DbParameter>();
-
-            if (param != null)
-            {
-                foreach (var item in param)
-                {
-                    var temp = DbProviderFactories.GetFactory(config.ProviderName).CreateParameter();
-                    temp.Value = item.Value == null ? DBNull.Value : item.Value;
-                    temp.ParameterName = item.ParameterName;
-                    temp.Direction = item.Direction;
-                    temp.DbType = item.DbType;
-                    list.Add(temp);
-                }
-            }
-
-            return list;
-        }
-        #endregion
-
         #region DbParameter合并
         /// <summary>
         /// DbParameter合并
