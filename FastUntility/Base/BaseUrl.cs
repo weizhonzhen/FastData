e1ddf90de4ce2e140ee0c00b7ed9cd28206d3438
weizhonzhen <czdswzz@126.com>
2020-08-03 08:44:39 +0800

    Update BaseUrl.cs

--- FastUntility/Base/BaseUrl.cs
+++ FastUntility/Base/BaseUrl.cs
@@ -12,13 +12,23 @@ namespace FastUntility.Base
     /// </summary>
     public static class BaseUrl
     {
-        private static readonly HttpClient http;
+        private static readonly Lazy<HttpClient> conn;
 
         static BaseUrl()
         {
-            http = new HttpClient(new HttpClientHandler() { AutomaticDecompression = DecompressionMethods.GZip });
-            http.DefaultRequestHeaders.Connection.Add("keep-alive");
+            conn = new Lazy<HttpClient>(() => new HttpClient(new HttpClientHandler() { AutomaticDecompression = DecompressionMethods.GZip }));
         }
+
+        internal static HttpClient http
+        {
+            get
+            {
+                var http = conn.Value;
+                http.DefaultRequestHeaders.Connection.Add("keep-alive");
+                return http;
+            }
+        }
+
 
         #region get url(select)
         /// <summary>
@@ -223,19 +233,3 @@ namespace FastUntility.Base
         #endregion
     }
 }
-
-/*
-    GET  请求获取由Request-URI所标识的资源。
-         
-    POST  在Request-URI所标识的资源后附加新的数据。
-         
-    HEAD  请求获取由Request-URI所标识的资源的响应消息报头。
-         
-    OPTIONS  请求查询服务器的性能，或查询与资源相关的选项和需求。
-         
-    PUT   请求服务器存储一个资源，并用Request-URI作为其标识。
-         
-    DELETE  请求服务器删除由Request-URI所标识的资源。
-         
-    TRACE  请求服务器回送收到的请求信息，主要用语测试或诊断。         
- */
