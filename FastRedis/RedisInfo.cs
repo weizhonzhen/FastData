c04f43b400dd5d6bea2fe6849f7da50434535c8b
weizhonzhen <czdswzz@126.com>
2020-10-21 10:43:58 +0800

    Add files via upload

--- FastRedis/RedisInfo.cs
+++ FastRedis/RedisInfo.cs
@@ -685,50 +685,41 @@ namespace FastRedis
             SaveLog(string.Format("方法：{0},键：{1},出错详情：{2}", CurrentMethod, key, ex.ToString()), "redis_exp");
         }
         #endregion
-        
+
         #region 写日志
         /// <summary>
         /// 说明：写日记
         /// </summary>
         /// <param name="StrContent">日志内容</param>
-        private static void SaveLog(string logContent, string fileName, string headName = "", bool IsWrap = false, int logCount = 10)
+        private static void SaveLog(string logContent, string fileName)
         {
-            var path = string.Format("{0}/App_Data/log/{1}", AppDomain.CurrentDomain.BaseDirectory, DateTime.Now.ToString("yyyy-MM"));
+            var path = string.Format("{0}/App_Data/log/{1}/{2}", AppDomain.CurrentDomain.BaseDirectory, DateTime.Now.ToString("yyyy-MM"), DateTime.Now.ToString("yyyy-MM-dd"));
 
-            try
+            if (!Directory.Exists(path))
+                Directory.CreateDirectory(path);
+
+            if (fileName == "")
+                fileName = string.Format("{0}.txt", DateTime.Now.ToString("yyyy-MM-dd-HH"));
+            else
+                fileName = string.Format("{0}_{1}.txt", fileName, DateTime.Now.ToString("yyyy-MM-dd-HH"));
+
+            path = string.Format("{0}/{1}", path, fileName);
+
+            if (!File.Exists(path))
+                using (var fs = File.Create(path)) { }
+
+            //写日志
+            using (var fs = new FileStream(path, FileMode.Append, FileAccess.Write, FileShare.ReadWrite))
             {
-                logCount--;
-
-                //新建文件
-                if (!Directory.Exists(path))
-                    Directory.CreateDirectory(path);
-
-                if (fileName == "")
-                    fileName = string.Format("{0}.txt", DateTime.Now.ToString("yyyy-MM-dd-HH"));
-                else
-                    fileName = string.Format("{0}_{1}.txt", fileName, DateTime.Now.ToString("yyyy-MM-dd-HH"));
-
-                //写日志
-                using (var fs = new FileStream(string.Format("{0}/{1}", path, fileName), FileMode.OpenOrCreate, FileAccess.Write))
+                using (var m_streamWriter = new StreamWriter(fs))
                 {
-                    var m_streamWriter = new StreamWriter(fs);
                     m_streamWriter.BaseStream.Seek(0, SeekOrigin.End);
-                    m_streamWriter.WriteLine(string.Format("{0}[{1}]{2}", headName, DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff"), logContent));
+                    m_streamWriter.WriteLine(string.Format("[{0}]{1}", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff"), logContent));
                     m_streamWriter.WriteLine("");
-
-                    if (IsWrap)
-                        m_streamWriter.WriteLine("");
-
                     m_streamWriter.Flush();
                     m_streamWriter.Close();
-                    m_streamWriter.Dispose();
                     fs.Close();
                 }
-            }
-            catch
-            {
-                if (logCount != 0)
-                    SaveLog(fileName, path, headName, IsWrap, logCount--);
             }
         }
         #endregion
