eea40706ea8c23fa2bcc9bdfd6be38e2cd8f7e3a
weizhonzhen <czdswzz@126.com>
2021-03-02 10:54:29 +0800

    Add files via upload

--- FastData/Base/MapXml.cs
+++ FastData/Base/MapXml.cs
@@ -333,7 +333,7 @@ namespace FastData.Base
                 #endregion
             }
 
-            if (DbCache.Get(cacheType, name.ToLower()).ToInt(0) > 0)
+            if (DbCache.Get(cacheType, name.ToLower()).ToInt(0) ==1)
                 param = tempParam.ToArray();
             return sql.ToString();
         }
