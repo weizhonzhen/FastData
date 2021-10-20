2f8ea9ff4c99e18a544ea8aef13c6aabc8f3bf8a
weizhonzhen <czdswzz@126.com>
2021-10-20 16:51:59 +0800

    Add files via upload

--- FastData/Check/BaseTable.cs
+++ FastData/Check/BaseTable.cs
@@ -543,7 +543,7 @@ namespace FastData.Check
                     sql = "select constraint_name PK from INFORMATION_SCHEMA.KEY_COLUMN_USAGE where upper(TABLE_NAME)=?tableName and constraint_name='PRIMARY' and upper(column_name)=?colName";
                 }
 
-                return db.ExecuteSqlList(sql, param.ToArray(), item.Config.IsOutSql,false).DicList.First() ?? new Dictionary<string, object>();
+                return db.ExecuteSqlList(sql, param.ToArray(), item.Config.IsOutSql,false).DicList.FirstOrDefault() ?? new Dictionary<string, object>();
             }
         }
         #endregion
