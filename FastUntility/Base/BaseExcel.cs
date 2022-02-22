8eee6afe438f3b5f9701c3a0b6b358bf6c55db92
weizhonzhen <czdswzz@126.com>
2022-02-22 09:39:40 +0800

    Add files via upload

--- FastUntility/Base/BaseExcel.cs
+++ FastUntility/Base/BaseExcel.cs
@@ -209,7 +209,7 @@ namespace FastUntility.Base
                         int j = 0;
                         foreach (var temp in item)
                         {
-                            if (string.Compare( temp.Key, exclude,false)==0)
+                            if (string.Compare( temp.Key, exclude, true) ==0)
                                 continue;
 
                             model.cell = model.row.CreateCell(j++);
--- FastUntility/Base/BaseMap.cs
+++ FastUntility/Base/BaseMap.cs
@@ -32,9 +32,9 @@ namespace FastUntility.Base
             }
 
             BaseDic.PropertyInfo<T1>().ForEach(m => {
-                if (list.Exists(a =>string.Compare( a.Name, m.Name,false)==0))
+                if (list.Exists(a =>string.Compare( a.Name, m.Name, true) ==0))
                 {
-                    var property = list.Find(a =>string.Compare( a.Name, m.Name,false)==0);
+                    var property = list.Find(a =>string.Compare( a.Name, m.Name, true) ==0);
                     var isList = m.PropertyType.GetGenericArguments().Length > 0;
                     var isLeafSystemType = isList && m.PropertyType.GetGenericArguments()[0].FullName.StartsWith("System.");
                     var isSystemType = m.PropertyType.FullName.StartsWith("System.");
@@ -54,7 +54,7 @@ namespace FastUntility.Base
                                 temp.GetType().GetProperties().ToList().ForEach(p => {
                                     if (propertyList.Exists(a => a.Name == p.Name))
                                     {
-                                        var tempProperty = propertyList.Find(a => string.Compare( a.Name, p.Name,false)==0);
+                                        var tempProperty = propertyList.Find(a => string.Compare( a.Name, p.Name, true) ==0);
                                         tempProperty.SetValue(leafModel, p.GetValue(temp));
                                     }
                                 });
@@ -79,7 +79,7 @@ namespace FastUntility.Base
                         (m.PropertyType as TypeInfo).GetProperties().ToList().ForEach(p => {
                             if (propertyList.Exists(a => a.Name == p.Name))
                             {
-                                var temp = propertyList.Find(a =>string.Compare( a.Name,p.Name,false)==0);
+                                var temp = propertyList.Find(a =>string.Compare( a.Name,p.Name, true) ==0);
                                 temp.SetValue(leafModel, p.GetValue(tempModel));
                             }
                         });
@@ -89,9 +89,9 @@ namespace FastUntility.Base
                 }
                 else
                 {
-                    if (dic.ToList().Exists(n => string.Compare( (n.Value as MemberExpression).Member.Name, m.Name,false)==0))
+                    if (dic.ToList().Exists(n => string.Compare( (n.Value as MemberExpression).Member.Name, m.Name, true) ==0))
                     {
-                        var temp = dic.ToList().Find(n => string.Compare( (n.Value as MemberExpression).Member.Name, m.Name,false)==0);
+                        var temp = dic.ToList().Find(n => string.Compare( (n.Value as MemberExpression).Member.Name, m.Name, true) ==0);
                         BaseEmit.Set<T>(result, temp.Key.Name, BaseEmit.Get<T1>(model, (temp.Value as MemberExpression).Member.Name));
                     }
                 }
