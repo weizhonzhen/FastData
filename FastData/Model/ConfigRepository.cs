683f96341bb316090de3f23bab8346dbdb1a1816
weizhonzhen <czdswzz@126.com>
2022-08-23 13:18:07 +0800

    Add files via upload

---
+++ FastData/Model/ConfigRepository.cs
@@ -0,0 +1,15 @@
+﻿using FastAop;
+
+namespace FastData.Model
+{
+    public class ConfigRepository
+    {
+        public FastAopAttribute Aop { get; set; }
+
+        public string NameSpaceModel { get; set; }
+
+        public string NameSpaceServie { get; set; }
+
+        public FastAop.FastAop.WebType webType { get; set; }
+    }
+}
