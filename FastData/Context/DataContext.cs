b43450024ed79ac903835feb2e456c25275557a8
weizhonzhen <czdswzz@126.com>
2024-07-10 09:50:12 +0800

    Add files via upload

--- FastData/Context/DataContext.cs
+++ FastData/Context/DataContext.cs
@@ -236,13 +236,13 @@ namespace FastData.Context
                             if (config.DbType == DataDbType.Oracle && !a.IsList)
                                 sql.Append(" and rownum <=1");
                             else if (config.DbType == DataDbType.DB2 && !a.IsList)
-                                sql.Append(" and fetch first 1 rows only");
+                                sql.Append(" fetch first 1 rows only");
                             else if (config.DbType == DataDbType.MySql && !a.IsList)
-                                sql.Append(" and limit 1");
+                                sql.Append(" limit 1");
                             else if (config.DbType == DataDbType.PostgreSql && !a.IsList)
-                                sql.Append(" and limit 1");
+                                sql.Append(" limit 1");
                             else if (config.DbType == DataDbType.SQLite && !a.IsList)
-                                sql.Append(" and limit 0 offset 1");
+                                sql.Append(" limit 0 offset 1");
 
                             cmd.CommandText = sql.ToString();
 
@@ -422,13 +422,13 @@ namespace FastData.Context
                 if (item.Config.DbType == DataDbType.Oracle && item.Take != 0)
                     sql.AppendFormat(" and rownum <={0}", item.Take);
                 else if (item.Config.DbType == DataDbType.DB2 && item.Take != 0)
-                    sql.AppendFormat(" and fetch first {0} rows only", item.Take);
+                    sql.AppendFormat(" fetch first {0} rows only", item.Take);
                 else if (item.Config.DbType == DataDbType.MySql && item.Take != 0)
-                    sql.AppendFormat(" and limit {0}", item.Take);
+                    sql.AppendFormat(" limit {0}", item.Take);
                 else if (item.Config.DbType == DataDbType.PostgreSql && item.Take != 0)
-                    sql.AppendFormat(" and limit {0}", item.Take);
+                    sql.AppendFormat(" limit {0}", item.Take);
                 else if (item.Config.DbType == DataDbType.SQLite && item.Take != 0)
-                    sql.AppendFormat(" and limit 0 offset {0}", item.Take);
+                    sql.AppendFormat(" limit 0 offset {0}", item.Take);
 
                 if (item.Predicate[0].Param.Count != 0)
                     param.AddRange(item.Predicate[0].Param);
@@ -521,13 +521,13 @@ namespace FastData.Context
                 if (item.Config.DbType == DataDbType.Oracle && item.Take != 0)
                     sql.AppendFormat(" and rownum <={0}", item.Take);
                 else if (item.Config.DbType == DataDbType.DB2 && item.Take != 0)
-                    sql.AppendFormat(" and fetch first {0} rows only", item.Take);
+                    sql.AppendFormat(" fetch first {0} rows only", item.Take);
                 else if (item.Config.DbType == DataDbType.MySql && item.Take != 0)
-                    sql.AppendFormat(" and limit {0}", item.Take);
+                    sql.AppendFormat(" limit {0}", item.Take);
                 else if (item.Config.DbType == DataDbType.PostgreSql && item.Take != 0)
-                    sql.AppendFormat(" and limit {0}", item.Take);
+                    sql.AppendFormat(" limit {0}", item.Take);
                 else if (item.Config.DbType == DataDbType.SQLite && item.Take != 0)
-                    sql.AppendFormat(" and limit 0 offset {0}", item.Take);
+                    sql.AppendFormat(" limit 0 offset {0}", item.Take);
 
                 if (item.Predicate[0].Param.Count != 0)
                     param.AddRange(item.Predicate[0].Param);
@@ -979,13 +979,13 @@ namespace FastData.Context
                 if (item.Config.DbType == DataDbType.Oracle && item.Take != 0)
                     sql.AppendFormat(" and rownum <={0}", item.Take);
                 else if (item.Config.DbType == DataDbType.DB2 && item.Take != 0)
-                    sql.AppendFormat(" and fetch first {0} rows only", item.Take);
+                    sql.AppendFormat(" fetch first {0} rows only", item.Take);
                 else if (item.Config.DbType == DataDbType.MySql && item.Take != 0)
-                    sql.AppendFormat(" and limit {0}", item.Take);
+                    sql.AppendFormat(" limit {0}", item.Take);
                 else if (item.Config.DbType == DataDbType.PostgreSql && item.Take != 0)
-                    sql.AppendFormat(" and limit {0}", item.Take);
+                    sql.AppendFormat(" limit {0}", item.Take);
                 else if (item.Config.DbType == DataDbType.SQLite && item.Take != 0)
-                    sql.AppendFormat(" and limit 0 offset {0}", item.Take);
+                    sql.AppendFormat(" limit 0 offset {0}", item.Take);
 
                 if (item.Predicate[0].Param.Count != 0)
                     param.AddRange(item.Predicate[0].Param);
@@ -1304,13 +1304,13 @@ namespace FastData.Context
                 if (item.Config.DbType == DataDbType.Oracle && item.Take != 0)
                     sql.AppendFormat(" and rownum <={0}", item.Take);
                 else if (item.Config.DbType == DataDbType.DB2 && item.Take != 0)
-                    sql.AppendFormat(" and fetch first {0} rows only", item.Take);
+                    sql.AppendFormat(" fetch first {0} rows only", item.Take);
                 else if (item.Config.DbType == DataDbType.PostgreSql && item.Take != 0)
-                    sql.AppendFormat(" and limit {0}", item.Take);
+                    sql.AppendFormat(" limit {0}", item.Take);
                 else if (item.Config.DbType == DataDbType.MySql && item.Take != 0)
-                    sql.AppendFormat(" and limit {0}", item.Take);
+                    sql.AppendFormat(" limit {0}", item.Take);
                 else if (item.Config.DbType == DataDbType.SQLite && item.Take != 0)
-                    sql.AppendFormat(" and limit 0 offset {0}", item.Take);
+                    sql.AppendFormat(" limit 0 offset {0}", item.Take);
 
                 if (item.Predicate[0].Param.Count != 0)
                     param.AddRange(item.Predicate[0].Param);
@@ -1400,13 +1400,13 @@ namespace FastData.Context
                 if (item.Config.DbType == DataDbType.Oracle && item.Take != 0)
                     sql.AppendFormat(" and rownum <={0}", item.Take);
                 else if (item.Config.DbType == DataDbType.DB2 && item.Take != 0)
-                    sql.AppendFormat(" and fetch first {0} rows only", item.Take);
+                    sql.AppendFormat(" fetch first {0} rows only", item.Take);
                 else if (item.Config.DbType == DataDbType.PostgreSql && item.Take != 0)
-                    sql.AppendFormat(" and limit {0}", item.Take);
+                    sql.AppendFormat(" limit {0}", item.Take);
                 else if (item.Config.DbType == DataDbType.MySql && item.Take != 0)
-                    sql.AppendFormat(" and limit {0}", item.Take);
+                    sql.AppendFormat(" limit {0}", item.Take);
                 else if (item.Config.DbType == DataDbType.SQLite && item.Take != 0)
-                    sql.AppendFormat(" and limit 0 offset {0}", item.Take);
+                    sql.AppendFormat(" limit 0 offset {0}", item.Take);
 
                 if (item.Predicate[0].Param.Count != 0)
                     param.AddRange(item.Predicate[0].Param);
